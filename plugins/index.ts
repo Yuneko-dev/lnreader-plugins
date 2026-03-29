25aa2c4a9efa308017c494eb3522d8edb541ce75
Elysia <71698422+aiko-chan-ai@users.noreply.github.com>
2026-03-29 18:57:38 +0700

    fix hako

--- plugins/index.ts
+++ plugins/index.ts
@@ -1,7 +1,5 @@
 import { Plugin } from '@/types/plugin';
-import p_0 from '@plugins/vietnamese/lightnovelvn.broken';
-import p_1 from '@plugins/vietnamese/LNHako';
-import p_2 from '@plugins/vietnamese/nettruyen.broken';
+import p_0 from '@plugins/vietnamese/LNHako';
 
-const PLUGINS: Plugin.PluginBase[] = [p_0, p_1, p_2];
+const PLUGINS: Plugin.PluginBase[] = [p_0];
 export default PLUGINS;
