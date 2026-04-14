78936cb9fd9ca561f5eceade9452e52502ab455e
FAFITS <famukonkiri@gmail.com>
2026-04-14 15:53:48 +0700

    UpVer

--- plugins/japanese/nocsyosetu.ts
+++ plugins/japanese/nocsyosetu.ts
@@ -11,7 +11,7 @@ class NocSyosetu implements Plugin.PagePlugin {
   name = 'NocSyosetu';
   icon = 'src/jp/nocsyosetu/icon.png';
   site = 'https://noc.syosetu.com/';
-  version = '1.1.2';
+  version = '1.1.3';
   headers = {
     'Cookie': 'over18=yes',
     'User-Agent':
